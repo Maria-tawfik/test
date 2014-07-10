dfbcdfc60d0432143114e61c85314ff74a955c0b
maria samy farid <miro19933991@gmail.com>
2014-07-11 00:14:15 +0200

    updated

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -2,9 +2,74 @@ require 'rails_helper'
 
 RSpec.describe "HomePages", :type => :request do
   describe "GET /home_pages" do
-    it "works! (now write some real specs)" do
-      get home_pages_path
-      expect(response.status).to be(200)
-    end
+
+  describe "Home page" do
+  	   it "Our Demo App" do
+
+  		   visit '/home_page/home'
+  		   page.should have_content ('Our Demo App')
+  	    end
+
   end
+
+  describe "About page" do
+  	it "About us" do
+
+  		visit '/home_page/about'
+  		page.should have_content ('About us')
+  	end
+  end
+
+  describe "Contact page" do
+  	it "Contact Us" do
+
+  		visit '/home_page/contact'
+  		page.should have_content ('Contact Us')
+  	end
+  end
+
+  describe "Location page" do
+  	it "Location" do
+
+  		visit '/home_page/location'
+  		page.should have_content ('Location')
+  	end
+  end
+
+  describe "Home page" do
+  	   it "Our Demo App" do
+
+  		   visit '/home_page/home'
+  		 expect(page).to have_title('Our Demo App')
+  	    end
+
+  end
+  
+  describe "About page" do
+  	it "About us" do
+
+  		visit '/home_page/about'
+  		expect(page).to have_title('About Us')
+   	end
+  end
+
+  describe "Location page" do
+  	it "Location" do
+
+  		visit '/home_page/location'
+  		expect(page).to have_title('Location')
+   	end
+  end
+  
+  describe "Contact page" do
+  	it "Contact us" do
+
+  		visit '/home_page/contact'
+  		expect(page).to have_title('Contact Us')
+   	end
+  end
+  
+
 end
+end
+
